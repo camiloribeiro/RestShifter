35ac4b4ce04c38b20b516e8a62e706db0bcd4ee6
Camilo Ribeiro <camilo@camiloribeiro.com>
2015-02-15 22:08:42 +0100

    Adding CHANGES file and new feature that supports post operations too

--- lib/RestShifter/Shifter.rb
+++ lib/RestShifter/Shifter.rb
@@ -12,8 +12,69 @@ end
 
 @shapes.all.each do |shape|
   service = @shapes.flavor shape.to_s.gsub!("@", "").to_sym
-  get service.path do
-    status service.status
-    service.body
+
+  if service.method_used == "get" 
+    if (service.request_accept.length > 0 && service.request_content_type.length > 0)
+      get service.path, :provides => service.request_accept, :consumes => service.request_content_type do
+        status service.response_status
+        content_type service.response_content_type
+        service.response_body
+      end
+
+    elsif (service.request_accept.length == 0 && service.request_content_type.length > 0)
+      get service.path, :consumes => service.request_content_type do
+        status service.response_status
+        content_type service.response_content_type
+        service.response_body
+      end
+
+    elsif  (service.request_accept.length > 0 && service.request_content_type.length == 0)
+      get service.path, :provides => service.request_accept do
+        binding.pry
+        status service.response_status
+        content_type service.response_content_type
+        service.response_body
+      end
+
+    elsif
+      get service.path do
+        status service.response_status
+        content_type service.response_content_type
+        service.response_body
+      end
+    end
+  end
+
+  if service.method_used == "post"
+    if (service.request_accept.length > 0 && service.request_content_type.length > 0)
+      post service.path, :provides => service.request_accept, :consumes => service.request_content_type do
+        status service.response_status
+        content_type service.response_content_type
+        service.response_body
+      end
+
+    elsif (service.request_accept.length == 0 && service.request_content_type.length > 0)
+      post service.path, :consumes => service.request_content_type do
+        status service.response_status
+        content_type service.response_content_type
+        service.response_body
+      end
+
+    elsif  (service.request_accept.length > 0 && service.request_content_type.length == 0)
+      post service.path, :provides => service.request_accept do
+        binding.pry
+        status service.response_status
+        content_type service.response_content_type
+        service.response_body
+      end
+
+    elsif
+      post service.path do
+        status service.response_status
+        content_type service.response_content_type
+        service.response_body
+      end
+    end
+
   end
 end
