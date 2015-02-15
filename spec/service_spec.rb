6908c3835e28aaf31f330caa1f57f5ce4b859733
Camilo Ribeiro <camilo@camiloribeiro.com>
2015-02-15 22:11:33 +0100

    Improving structure of the tests

--- spec/service_spec.rb
+++ spec/service_spec.rb
@@ -7,42 +7,46 @@ describe RestShifter do
     Sinatra::Application
   end
 
-  # Get
-  it "Service should return 200 for get" do
-    get '/ok'
-    expect(last_response.status).to eq(200)
-    expect(last_response.body).to eq("success")
-    expect(last_response.headers['Content-Type']).to eq("application/json")
+  describe "Get Operations" do
+    # Get
+    it "Service should return 200 for get" do
+      get '/ok'
+      expect(last_response.status).to eq(200)
+      expect(last_response.body).to eq("success")
+      expect(last_response.headers['Content-Type']).to eq("application/json")
+    end
+
+    it "Service should return 404 without accept defining routing for get" do
+      get '/notfound'
+      expect(last_response.status).to eq(404)
+      expect(last_response.body).to eq("Not Found")
+      expect(last_response.headers['Content-Type']).to eq("application/foo.company.product.type-version+json")
+    end
+
+    #  it "Service should return 404 with accept defining routing" do
+    #    header 'Accept', "application/foo.company.product.type-version2+json"
+    #    get '/notfound'
+    #    expect(last_response.status).to eq(404)
+    #    expect(last_response.body).to eq("Not Found with accept")
+    #    expect(last_response.headers['Content-Type']).to eq("application/foo.company.product.type-version2+json")
+    #  end
   end
+  describe "Post operations" do
 
-  it "Service should return 404 without accept defining routing for get" do
-    get '/notfound'
-    expect(last_response.status).to eq(404)
-    expect(last_response.body).to eq("Not Found")
-    expect(last_response.headers['Content-Type']).to eq("application/foo.company.product.type-version+json")
+    # Post
+    it "Service should return 200 for post" do
+      post '/ok'
+      expect(last_response.status).to eq(200)
+      expect(last_response.body).to eq("success")
+      expect(last_response.headers['Content-Type']).to eq("application/json")
+    end
+
+    it "Service should return 404 without accept defining routing for post" do
+      post '/notfound'
+      expect(last_response.status).to eq(404)
+      expect(last_response.body).to eq("Not Found")
+      expect(last_response.headers['Content-Type']).to eq("application/foo.company.product.type-version+json")
+    end
+
   end
-
-  # Post
-  it "Service should return 200 for post" do
-    post '/ok'
-    expect(last_response.status).to eq(200)
-    expect(last_response.body).to eq("success")
-    expect(last_response.headers['Content-Type']).to eq("application/json")
-  end
-
-  it "Service should return 404 without accept defining routing for post" do
-    post '/notfound'
-    expect(last_response.status).to eq(404)
-    expect(last_response.body).to eq("Not Found")
-    expect(last_response.headers['Content-Type']).to eq("application/foo.company.product.type-version+json")
-  end
-
-#  it "Service should return 404 with accept defining routing" do
-#    header 'Accept', "application/foo.company.product.type-version2+json"
-#    get '/notfound'
-#    expect(last_response.status).to eq(404)
-#    expect(last_response.body).to eq("Not Found with accept")
-#    expect(last_response.headers['Content-Type']).to eq("application/foo.company.product.type-version2+json")
-#  end
-
 end
