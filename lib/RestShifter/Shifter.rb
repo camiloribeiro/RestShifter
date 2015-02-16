07c00a957ca86a0d6289abc0c4588d6047e53e04
Camilo Ribeiro <camilo@camiloribeiro.com>
2015-02-16 01:26:36 +0100

    Refactoring Shifter

--- lib/RestShifter/Shifter.rb
+++ lib/RestShifter/Shifter.rb
@@ -8,72 +8,28 @@ module RestShifter; end
 class Shifter < Sinatra::Base
 end
 
-@shapes = IceCream::IceCream.new File.join(File.dirname(__FILE__), "../../spec/flavors")
+shapes = IceCream::IceCream.new File.join(File.dirname(__FILE__), "../../spec/flavors")
 
-@shapes.all.each do |shape|
-  service = @shapes.flavor shape.to_s.gsub!("@", "").to_sym
+services = []
+shapes.all.each do |shape|
+  services << shapes.flavor(shape.to_s.gsub!("@", "").to_sym)
+end
+
+services.each do |service|
 
   if service.method_used == "get" 
-    if (service.request_accept.length > 0 && service.request_content_type.length > 0)
-      get service.path, :provides => service.request_accept, :consumes => service.request_content_type do
-        status service.response_status
-        content_type service.response_content_type
-        service.response_body
-      end
-
-    elsif (service.request_accept.length == 0 && service.request_content_type.length > 0)
-      get service.path, :consumes => service.request_content_type do
-        status service.response_status
-        content_type service.response_content_type
-        service.response_body
-      end
-
-    elsif  (service.request_accept.length > 0 && service.request_content_type.length == 0)
-      get service.path, :provides => service.request_accept do
-        binding.pry
-        status service.response_status
-        content_type service.response_content_type
-        service.response_body
-      end
-
-    elsif
-      get service.path do
-        status service.response_status
-        content_type service.response_content_type
-        service.response_body
-      end
+    get service.path do
+      status service.response_status
+      content_type service.response_content_type
+      service.response_body
     end
   end
 
   if service.method_used == "post"
-    if (service.request_accept.length > 0 && service.request_content_type.length > 0)
-      post service.path, :provides => service.request_accept, :consumes => service.request_content_type do
-        status service.response_status
-        content_type service.response_content_type
-        service.response_body
-      end
-
-    elsif (service.request_accept.length == 0 && service.request_content_type.length > 0)
-      post service.path, :consumes => service.request_content_type do
-        status service.response_status
-        content_type service.response_content_type
-        service.response_body
-      end
-
-    elsif  (service.request_accept.length > 0 && service.request_content_type.length == 0)
-      post service.path, :provides => service.request_accept do
-        binding.pry
-        status service.response_status
-        content_type service.response_content_type
-        service.response_body
-      end
-
-    elsif
-      post service.path do
-        status service.response_status
-        content_type service.response_content_type
-        service.response_body
-      end
+    post service.path do
+      status service.response_status
+      content_type service.response_content_type
+      service.response_body
     end
 
   end
