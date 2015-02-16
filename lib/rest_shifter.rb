6770aaedf6669500cf4230212abfe529a8cc8070
Camilo Ribeiro <camilo@camiloribeiro.com>
2015-02-16 17:38:36 +0100

    Adding commands

--- lib/rest_shifter.rb
+++ lib/rest_shifter.rb
@@ -7,10 +7,15 @@ require 'sinatra'
 
 ##Modules
 require File.join(File.dirname(__FILE__), './rest_shifter/shifter.rb')
+require File.join(File.dirname(__FILE__), './rest_shifter/commands.rb')
 
 ##internal dependences
 
 ## commands
+require 'rest_shifter/commands'
+require File.join(File.dirname(__FILE__), './rest_shifter/commands/main')
+require File.join(File.dirname(__FILE__), './rest_shifter/commands/help')
+require File.join(File.dirname(__FILE__), './rest_shifter/commands/version')
 
 module RestShifter
 end
---
+++ lib/rest_shifter/commands.rb
@@ -0,0 +1,2 @@
+module RestShifter::Commands
+end
