ee2634a0131002f7fc1aac087550fe5a90d5df2a
Camilo Ribeiro <camilo@camiloribeiro.com>
2015-02-17 23:05:54 +0100

    Fixing issue on command create

--- lib/rest_shifter/commands/create.rb
+++ lib/rest_shifter/commands/create.rb
@@ -9,6 +9,7 @@ class RestShifter::Commands::Create
       File.open(dirname + "/" + name, 'w') {|f| f.write(
 'method_used                 = "get"
 path                        = "/hello_world"
+response_sleep              = 0
 response_status             = "200"
 response_body               = "{ \"hello_world\" : \"Your service is working fine. :D\" }"
 response_content_type       = "application/json"') }
